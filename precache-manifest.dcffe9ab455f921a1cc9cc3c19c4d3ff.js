b046d15f645e4e20b7068f3ac06e59d8c0319404
jaidelman <jaidelma@uoguelph.ca>
2020-08-24 14:49:10 -0400

    Updates

---
+++ precache-manifest.dcffe9ab455f921a1cc9cc3c19c4d3ff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb279a4fa91c695b00600b7f633f58f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "182c963ce062ee92c957",
+    "url": "/static/css/main.8d5e8e68.chunk.css"
+  },
+  {
+    "revision": "ae3aeab911ad3e25615b",
+    "url": "/static/js/2.bbf21a5e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bbf21a5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "182c963ce062ee92c957",
+    "url": "/static/js/main.bce38536.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  },
+  {
+    "revision": "313ac5b33404c634733eff35d85f6bea",
+    "url": "/static/media/email.313ac5b3.png"
+  },
+  {
+    "revision": "371a1a03898a0885edfd5229b17d6da3",
+    "url": "/static/media/instagram.371a1a03.png"
+  },
+  {
+    "revision": "1d640f4c2b631494486a0b795a9fa2d4",
+    "url": "/static/media/linkedin.1d640f4c.png"
+  },
+  {
+    "revision": "d385d71c72b599f71550836d632f8a96",
+    "url": "/static/media/revyoome.d385d71c.png"
+  },
+  {
+    "revision": "13d5e900d678f4198ee7b8e638b3e2f8",
+    "url": "/static/media/typing.13d5e900.jpeg"
+  },
+  {
+    "revision": "3be8012142e0fc18d2b36783400e6290",
+    "url": "/static/media/working.3be80121.png"
+  }
+]);
